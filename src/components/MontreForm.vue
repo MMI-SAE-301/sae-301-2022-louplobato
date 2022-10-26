2aea65c6cbc5f007e914d80d2fb63836f9002bc6
llobato <louplelpro@gmail.com>
2022-10-26 09:20:11 +0200

    Flex Form Montre

--- src/components/MontreForm.vue
+++ src/components/MontreForm.vue
@@ -17,58 +17,67 @@ const gardetemps = ref<montre>(props.data ?? {});
         <MontreSvg v-bind="gardetemps" id="montresvg" class="w-96" />
     </div>
     <FormKit type="form" v-model="gardetemps">
-        <div class="flex flex-col items-center">
-            <p class="my-6 font-Quick">
-                CORNES
-            </p>
-            <FormKit name="cornes" value="#FFFFFF" type="radio" :options="materiaux"
-                :sections-schema="{ inner: { $el: null }, decorator: { $el: null } }" input-class="peer sr-only"
-                options-class="flex justify-between w-64">
-                <template #label="context">
-                    <div class="flex flex-col items-center ">
-                        <div class="h-11 w-11 rounded-full border-2 peer-checked:border-grey-900"
-                            :style="{ backgroundColor: context.options.value }">
+        <div class="flex flex-col items-center gap-3">
+            <div class="flex flex-col items-center">
+                <p class="my-6 font-Quick text-5xl">
+                    CORNES
+                </p>
+                <FormKit name="cornes" value="#FFFFFF" type="radio" :options="materiaux"
+                    :sections-schema="{ inner: { $el: null }, decorator: { $el: null } }" input-class="peer sr-only"
+                    options-class="flex justify-between w-64">
+                    <template #label="context">
+                        <div class="flex flex-col items-center ">
+                            <div class="h-11 w-11 rounded-full border-2 peer-checked:border-grey-900"
+                                :style="{ backgroundColor: context.options.value }">
+                            </div>
+                            <span class="font-Quick">{{ context.option.label }}</span>
                         </div>
-                        <span class="font-Quick">{{ context.option.label }}</span>
-                    </div>
-                </template>
-            </FormKit>
+                    </template>
+                </FormKit>
+            </div>
+
+            <div class="flex flex-col items-center">
+                <p class="my-6 font-Quick text-5xl">
+                    LUNETTE
+                </p>
+                <FormKit name="lunette" value="#FFFFFF" type="radio" :options="materiaux"
+                    :sections-schema="{ inner: { $el: null }, decorator: { $el: null } }" input-class="peer sr-only"
+                    options-class="flex justify-between w-64">
+                    <template #label="context">
+                        <div class="flex flex-col items-center ">
+                            <div class="h-11 w-11 rounded-full border-2 peer-checked:border-grey-900"
+                                :style="{ backgroundColor: context.options.value }">
+                            </div>
+                            <span class="font-Quick">{{ context.option.label }}</span>
+                        </div>
+                    </template>
+                </FormKit>
+            </div>
+            <div class="flex flex-col items-center">
+                <p class="my-6 font-Quick text-5xl">
+                    BOITIER
+                </p>
+                <FormKit name="boitier" value="#FFFFFF" type="radio" :options="materiaux"
+                    :sections-schema="{ inner: { $el: null }, decorator: { $el: null } }" input-class="peer sr-only"
+                    options-class="flex justify-between w-64">
+                    <template #label="context">
+                        <div class="flex flex-col items-center ">
+                            <div class="h-11 w-11 rounded-full border-2 peer-checked:border-grey-900"
+                                :style="{ backgroundColor: context.options.value }">
+                            </div>
+                            <span class="font-Quick">{{ context.option.label }}</span>
+                        </div>
+                    </template>
+                </FormKit>
+            </div>
+            <div class="flex flex-col items-center">
+                <p class="my-6 font-Quick text-5xl">
+                    BRACELET
+                </p>
+                <FormKit name="bracelet" value="#FFFFFF" type="color" />
+            </div>
+
         </div>
 
-        <div class="flex flex-col items-center">
-            <p class="my-6 font-Quick">
-                LUNETTE
-            </p>
-            <FormKit name="lunette" value="#FFFFFF" type="radio" :options="materiaux"
-                :sections-schema="{ inner: { $el: null }, decorator: { $el: null } }" input-class="peer sr-only"
-                options-class="flex justify-between w-64">
-                <template #label="context">
-                    <div class="flex flex-col items-center ">
-                        <div class="h-11 w-11 rounded-full border-2 peer-checked:border-grey-900"
-                            :style="{ backgroundColor: context.options.value }">
-                        </div>
-                        <span class="font-Quick">{{ context.option.label }}</span>
-                    </div>
-                </template>
-            </FormKit>
-        </div>
-        <div class="flex flex-col items-center">
-            <p class="my-6 font-Quick">
-                BOITIER
-            </p>
-            <FormKit name="boitier" value="#FFFFFF" type="radio" :options="materiaux"
-                :sections-schema="{ inner: { $el: null }, decorator: { $el: null } }" input-class="peer sr-only"
-                options-class="flex justify-between w-64">
-                <template #label="context">
-                    <div class="flex flex-col items-center ">
-                        <div class="h-11 w-11 rounded-full border-2 peer-checked:border-grey-900"
-                            :style="{ backgroundColor: context.options.value }">
-                        </div>
-                        <span class="font-Quick">{{ context.option.label }}</span>
-                    </div>
-                </template>
-            </FormKit>
-        </div>
-        <FormKit name="bracelet" label="bracelet" value="#FFFFFF" type="color" />
     </FormKit>
 </template>
