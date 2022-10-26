23bdbee27149c4639d56c172845dee16dfe32003
llobato <louplelpro@gmail.com>
2022-10-26 15:55:48 +0200

    création composant login

---
+++ src/components/Login.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { supabase, user } from "@/supabase";
+import nodeTest from "node:test";
+const newuser = ref(false);
+async function signIn(data, node) {
+    const { user, error } = await (newuser.value
+        ? supabase.auth.signUp(data)
+        : supabase.auth.signIn(data));
+
+    if (error) {
+        console.error(error);
+        node.setErrors([error.message]);
+    }
+}
+</script>
