f6cb7ad876240822b867f704fc561368e8b41858
llobato <louplelpro@gmail.com>
2022-10-27 09:11:00 +0200

    Edit Montre fonctionnel

---
+++ src/pages/montre/edit/[id].vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import FormMontre from "@/components/MontreForm.vue";
+
+defineProps<{
+    id: string;
+}>();
+</script>
+<template>
+    <FormMontre :id="id" />
+</template>
